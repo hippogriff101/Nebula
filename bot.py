784764ccf5f73a5facf03c148d48bd46df86df50
hippogriff101 <143739931+hippogriff101@users.noreply.github.com>
2026-04-11 22:32:37 +0100

    Refactor command handling for idea and theme to improve date validation and response clarity

--- bot.py
+++ bot.py
@@ -34,48 +34,49 @@ def command(ack, say, respond, command):
 
     today = date.today()
     if today < anchor or today > end_date:
-        respond(f"This command is only available from {anchor.isoformat()} to {end_date.isoformat()}.")
+        respond("This command is only available from 15th April to 30th April.")
         return
 
     if user_text == "idea":
-        print("Idea being generated!")
-        respond("Hey stargazer, I got your Project Request. Just cooking it up, it'll be with you soon!")
-        response = client.chat.send(
-        model="qwen/qwen3-32b",
-        messages=[
-            {"role": "user", "content": 
-            "You are a slack bot designed to generate one short (1-2 sentacnes) and creative project idea for a event called Nebula."
-            "Please be very carful with formatting, make sure you do not EVER Markdown and avoid using an exess of emojis!"
-            "This weeks theme is: " + weeks_theme
-            }
-        ],
-        stream=False,
-    )
-        say("Yo, <@{user_id}>, here is your idea: " + response.choices[0].message.content)
-        print("Sent to channel!")
-        return
-    if user_text == "joke":
-        print("somethin of a joker")
-        response = client.chat.send(
-        model="qwen/qwen3-32b",
-        messages=[
-            {"role": "user", "content": 
-            "You are a slack bot designed to generate one short space themed joke!"
-            "Please be very carful with formatting, make sure you do not EVER Markdown and avoid using an exess of emojis!"
-            }
-        ],
-        stream=False,
-    )
-        respond("Here is your space joke!: " + response.choices[0].message.content)        
+        today = date.today()
+        if today < anchor or today > end_date:
+            respond("This command is only available from 15th April to 30th April.")
+            return
+        else:
+            print("Idea being generated!")
+            respond("Hey stargazer, I got your Project Request. Just cooking it up, it'll be with you soon!")
+            response = client.chat.send(
+            model="qwen/qwen3-32b",
+            messages=[
+                {"role": "user", "content": 
+                "You are a slack bot designed to generate one short (1-2 sentacnes) and creative project idea for a event called Nebula."
+                "Please be very carful with formatting, make sure you do not EVER Markdown and avoid using an exess of emojis!"
+                "This weeks theme is: " + weeks_theme
+                }
+            ],
+            stream=False,
+        )
+            say("Yo, <@{user_id}>, here is your idea: " + response.choices[0].message.content)
+            print("Sent to channel!")
+            return
+      
     if user_text == "theme":
         print("We got a THEMER!")
-        respond("This cycles's theme is: " + weeks_theme)
-        return
+        today = date.today()
+        if today < anchor or today > end_date:
+            respond(f"This command is only available from {anchor.isoformat()} to {end_date.isoformat()}.")
+            return
+        else:
+            respond("This cycles's theme is: " + weeks_theme)
+            return
+
     if user_text == "admin":
         respond("*405 : FORBIDEN* - Don't press buttons if you don't know what they do :meow:")
-        respond
+        return
+    
     else:
         respond("Hey, I'm Nebula Bot! A little slack bot made by <@freddie> for <#nebula>! Unfortunatly just typing my command doesn't do anything; you can do `/nebula theme` to find out this cycles theme or `/nebula idea` to get project ideas!")
+
 if __name__ == "__main__":
     SocketModeHandler(
         app,
